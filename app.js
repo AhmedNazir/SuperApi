f95b240bca3aeca8e0ed47011f433b49483f1853
Ahmed Nazir <AhmedNazir@users.noreply.github.com>
2022-09-03 11:18:49 +0600

    a

---
+++ app.js
@@ -0,0 +1,39 @@
+// External Module
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv").config();
+const cors = require("cors");
+
+// Internal Modules
+const indexRouter = require("./routes/indexRouter");
+const randomRouter = require("./routes/randomRouter");
+const weatherRouter = require("./routes/weatherRouter");
+const shortenerRouter = require("./routes/shortenerRouter");
+const newsRouter = require("./routes/newsRouter");
+
+// App Module
+const app = express();
+
+// Database Connection
+mongoose
+    .connect(process.env.DATABASE_CONNECTION_PATH)
+    .then(() => console.log("Database connection is ok"))
+    .catch((err) => console.log(err.message));
+
+// App Setting
+app.use(express.json());
+app.use(cors());
+
+// Route Setting
+app.use("/", indexRouter);
+app.use("/random", randomRouter);
+app.use("/shortener", shortenerRouter);
+app.use("/weather", weatherRouter);
+app.use("/news", newsRouter);
+
+// Server Listen
+app.listen(process.env.PORT || 3000, (err) => {
+    if (err) console.log("Server setup is failed");
+    else
+        console.log(`App is listening at http://localhost:${process.env.PORT}`);
+});
