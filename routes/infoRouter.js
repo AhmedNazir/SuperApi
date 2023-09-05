098e54637195ba27aa98a4e337622dd96f8bca87
Ahmed Nazir <AhmedNazir@users.noreply.github.com>
2023-09-05 09:57:00 +0000

    yy

--- routes/infoRouter.js
+++ routes/infoRouter.js
@@ -6,29 +6,38 @@ const geoip = require('geoip-lite');
 const router = express.Router();
 
 
-router.get('/', function(req, res){
+router.get('/', async function(req, res){
 
     try {
         const geo = geoip.lookup(req.ip);
+        const url = `https://ipinfo.io/${req.ip}?token=d1d067681e8dcb`;
 
+        let result = await axios.get(url);
+        if (!result.data) throw new Error("Weather data collection is failed");
+        result = result.data;
+
+        
         res.status(200).json({
             error: false,
             data: {
                 Headers: JSON.stringify(req.headers),
-                IP: JSON.stringify(req.ip),
+                IP: req.ip,
                 Browser: req.headers["user-agent"],
                 Language: req.headers["accept-language"],
                 Country: (geo ? geo.country: "Unknown"),
                 Region: (geo ? geo.region: "Unknown"),
+                ipinfo: result,
             },
         });
 
     } catch (error) {
         res.status(500).json({
-            error: true,
+            error: error.message || "unknown error",
         });
     }
 
 });
+
+router.info
 
 module.exports = router;
