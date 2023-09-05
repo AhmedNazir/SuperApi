16617b7c0f7934dbe9b53599e6b8b7f2105365b5
Ahmed Nazir <AhmedNazir@users.noreply.github.com>
2023-09-05 10:01:08 +0000

    ip

--- routes/infoRouter.js
+++ routes/infoRouter.js
@@ -21,7 +21,7 @@ router.get('/', async function(req, res){
         res.status(200).json({
             error: false,
             data: {
-                Headers: JSON.stringify(req.headers),
+                // Headers: JSON.stringify(req.headers),
                 IP: req.ip,
                 Browser: req.headers["user-agent"],
                 Language: req.headers["accept-language"],
@@ -38,6 +38,32 @@ router.get('/', async function(req, res){
     }
 
 });
+
+router.get('/:ip', async function(req, res){
+
+    try {
+        const url = `https://ipinfo.io/${req.params.ip}?token=d1d067681e8dcb`;
+
+        let result = await axios.get(url);
+        if (!result.data) throw new Error("Weather data collection is failed");
+        result = result.data;
+
+        
+        res.status(200).json({
+            error: false,
+            data: {
+                ipinfo: result,
+            },
+        });
+
+    } catch (error) {
+        res.status(500).json({
+            error: error.message || "unknown error",
+        });
+    }
+
+});
+
 
 router.info
 
